ec5759e285420931283c412bc73f73783f8f747a
chevendrasoujanya <kesar989@gmail.com>
2016-12-05 10:00:48 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Code to find Area of a Circle
+var radius = 10;
+var area = 22/7 * radius * radius; // Area of a circle formula = PI * r * r (PI=22/7)
+console.log("Area of a Circle: " + area);
+
+// Code to find Greatest Number among 3 Numbers
+var a = 10, b = 15, c = 5;
+
+if(a > b && a > c)
+{
+	console.log("a is the Greatest Number. and Its value is: " +a)
+} 
+else if( b > a && b > c)
+{
+	console.log("b is the Greatest Number. and Its value is: " +b)
+}
+else
+{
+	console.log("c is the Greatest Number. and Its value is: " +c)	
+}
